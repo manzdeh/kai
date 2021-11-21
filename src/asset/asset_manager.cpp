610065ab7dcb45d13d95067b73a0255b17ad7526
Amanch Esmailzadeh <5677914+manzdeh@users.noreply.github.com>
2021-11-21 17:28:49 +0100

    use the manifest to handle asset loading and management

--- src/asset/asset_manager.cpp
+++ src/asset/asset_manager.cpp
@@ -4,16 +4,22 @@
  **************************************************/
 
 #include "asset_manager.h"
+
+#include "asset_table.h"
 #include "mesh.h"
 
 #include "../core/includes/alloc.h"
 #include "../core/includes/fileio.h"
+#include "../core/includes/math.h"
 #include "../core/includes/render.h"
 #include "../core/includes/system.h"
 
+#include <stddef.h>
+
 struct AssetManager {
-    struct HashTable {
+    struct HashTableEntry {
         AssetId key;
+        size_t refcount;
         void *value;
     };
 
@@ -26,25 +32,48 @@ struct AssetManager {
         page_count = bytes / page_size; // Every asset will occupy at least 1 unique page, so small assets should be packed together for optimal use
         pages = kai::reserve_pages(nullptr, page_count);
 
-        // The hash table is stored at the start of the address space that's reserved for assets
+        // The asset name lookup table (manifest) is stored at the start of the address space that's reserved for assets
+        kai::FileHandle asset_manifest = kai::open_file("data/asset_manifest.bin"); // TODO: Hardcoded for now, but this should probably be retrieved from some config
+        KAI_ASSERT(asset_manifest);
+
+        size_t manifest_size = kai::get_file_size(asset_manifest);
+        size_t asset_page_count = (manifest_size / page_size) + 1;
+        file_paths = static_cast<AssetTableHeader *>(kai::commit_pages(pages, asset_page_count));
+        kai::read_file(asset_manifest, file_paths);
+        kai::close_file(asset_manifest);
+
+        pages_used += asset_page_count;
+
+        // The hash table is stored immediately after the asset name lookup table
+        void *table_page_start = get_page(pages_used);
         table_entries = page_count;
-        size_t table_page_count = ((table_entries * sizeof(HashTable)) / page_size) + 1;
-        table = static_cast<HashTable *>(kai::commit_pages(pages, table_page_count));
+        size_t table_page_count = ((table_entries * sizeof(HashTableEntry)) / page_size) + 1;
+        table = static_cast<HashTableEntry *>(kai::commit_pages(table_page_start, table_page_count));
         pages_used += table_page_count;
 
-        next_reserved_page = static_cast<unsigned char *>(pages) + (table_page_count * page_size);
+        table_entries = kai::max<size_t>(0, table_entries - pages_used);
+
+        next_reserved_page = get_page(pages_used);
 
         // TODO: Need to keep track of a free-list of pages as well
     }
 
-    void * find(AssetId id) const;
-    void * insert(AssetId id, void *data);
+    void * get_page(size_t page_offset, void *start_page = nullptr) {
+        start_page = (start_page) ? start_page : pages;
+        return reinterpret_cast<void *>(reinterpret_cast<ptrdiff_t>(start_page) + (page_offset * kai::get_page_size()));
+    }
+
+    HashTableEntry * find(AssetId id, size_t *out_index = nullptr) const;
+    HashTableEntry * insert(AssetId id, void *data);
+    HashTableEntry * insert_at(size_t index, AssetId id, void *data);
+    void * commit_pages(size_t bytes);
 
     void linear_probe(size_t &index) const {
         index = (index + 1) % table_entries;
     }
 
-    HashTable *table = nullptr;
+    AssetTableHeader *file_paths = nullptr;
+    HashTableEntry *table = nullptr;
     size_t table_entries = 0;
     void *pages = nullptr;
     void *next_reserved_page = nullptr;
@@ -52,33 +81,38 @@ struct AssetManager {
     size_t pages_used = 0;
 };
 
-void * AssetManager::find(AssetId id) const {
-    void *value = nullptr;
+AssetManager::HashTableEntry * AssetManager::find(AssetId id, size_t *out_index) const {
+    AssetManager::HashTableEntry *entry = nullptr;
     size_t index = id % table_entries;
 
     while(table[index].key != 0) {
         if(table[index].key == id) {
-            value = table[index].value;
+            entry = &table[index];
             break;
         }
 
         linear_probe(index);
     }
 
-    return value;
+    if(out_index) {
+        *out_index = index;
+    }
+
+    return entry;
 }
 
-void * AssetManager::insert(AssetId id, void *data) {
+AssetManager::HashTableEntry * AssetManager::insert(AssetId id, void *data) {
     size_t index = id % table_entries;
     size_t count = 0;
 
     do {
         if(table[index].key == 0) {
             table[index].key = id;
+            table[index].refcount = 0;
             table[index].value = data;
-            return table[index].value;
+            return &table[index];
         } else if(table[index].key == id) {
-            return table[index].value;
+            return &table[index];
         }
 
         linear_probe(index);
@@ -86,6 +120,31 @@ void * AssetManager::insert(AssetId id, void *data) {
     } while(count < table_entries);
 
     return nullptr;
+}
+
+AssetManager::HashTableEntry * AssetManager::insert_at(size_t index, AssetId id, void *data) {
+    if(index < table_entries && table[index].key == 0) {
+        table[index].key = id;
+        table[index].refcount = 0;
+        table[index].value = data;
+        return &table[index];
+    }
+
+    return nullptr;
+}
+
+void * AssetManager::commit_pages(size_t bytes) {
+    // TODO: This needs to be done through (and validated against) a free-list of available pages,
+    // right now it simply bumps the page up by however many are needed and it could go out of bounds.
+    void * committed_pages = nullptr;
+
+    if(bytes > 0) {
+        size_t count = (bytes / kai::get_page_size()) + 1;
+        committed_pages = kai::commit_pages(next_reserved_page, count);
+        next_reserved_page = get_page(count, next_reserved_page);
+    }
+
+    return committed_pages;
 }
 
 static AssetManager asset_manager;
@@ -171,11 +230,54 @@ void destroy_asset_manager(void) {
     }
 }
 
-void * load_asset(AssetId /*id*/) {
-    // STUB
-    return nullptr;
+void * load_asset(AssetId id) {
+    size_t table_index;
+    AssetManager::HashTableEntry *asset = asset_manager.find(id, &table_index);
+
+    if(asset && asset->refcount > 0) {
+        asset->refcount++;
+        return asset->value;
+    }
+
+    AssetTableHeader *table = asset_manager.file_paths;
+
+    size_t index = (id ^ table->mod) % table->count;
+    ptrdiff_t start = reinterpret_cast<ptrdiff_t>(table) + offsetof(AssetTableHeader, buffer);
+    ptrdiff_t offset = *reinterpret_cast<ptrdiff_t *>(start + (index * sizeof(ptrdiff_t)));
+
+    const char *path = reinterpret_cast<const char *>(start + offset);
+
+    kai::FileHandle asset_file = kai::open_file(path);
+    void *asset_data = nullptr;
+
+    AssetManager::HashTableEntry *entry = asset_manager.insert_at(table_index, id, nullptr);
+    if(entry) {
+        asset_data = asset_manager.commit_pages(kai::get_file_size(asset_file));
+        KAI_ASSERT(asset_data);
+
+        kai::read_file(asset_file, asset_data);
+    }
+
+    kai::close_file(asset_file);
+
+    return asset_data;
 }
 
-void unload_asset(AssetId /*id*/) {
-    // STUB
+void unload_asset(AssetId id) {
+    AssetManager::HashTableEntry *entry = asset_manager.find(id);
+
+    if(entry) {
+        if(entry->refcount > 0) {
+            entry->refcount--;
+        }
+
+        if(entry->refcount == 0) {
+            // TODO: Remove entry from the hash table
+            memset(entry, 0, sizeof(*entry));
+        }
+    } else {
+#ifdef KAI_DEBUG
+        kai::log("Tried to unload a file with hash: \"%llu\", but the file was not loaded before", id);
+#endif
+    }
 }
